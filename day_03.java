2c23ab319614c93e5f5259ec2b5791df30e53bd8
Tomato <84717386+manager6@users.noreply.github.com>
2022-10-29 23:27:26 +0800

    Changes

---
+++ day_03.java
@@ -0,0 +1,19 @@
+public class day_03 {
+    public static void main(String[] args) {
+        double mianji1=gitmianji(4.1,2.2);
+        double mianji2=gitmianji(3.1,3.2);
+
+        if (mianji1>mianji2){
+            System.out.print("第一个长方形大");
+        }else {
+            System.out.print("第二个长方形大");
+        }
+
+
+    }
+    public static double gitmianji(double a,double b){
+        double mianji =a*b;
+        return mianji;
+
+    }
+}
