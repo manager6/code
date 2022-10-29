e7721a2a4043558e4069797ae17bdbadfafc7b88
Tomato <84717386+manager6@users.noreply.github.com>
2022-10-29 22:01:33 +0800

    Changes

--- day_01.java
+++ day_01.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class day_01 {
     public static void main(String[] args) {
-            
+        int[] arr = {1, 2, 3, 4, 5};
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int num = r.nextInt(arr.length);
+            int temp = arr[i];
+            arr[i] = arr[num];
+            arr[num] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf(arr[i] + " ");
+        }
+
+
     }
 }
