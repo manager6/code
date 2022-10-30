3f44efbfe620b027e2e1404a7ab611b5c10e22fe
Tomato <84717386+manager6@users.noreply.github.com>
2022-10-30 17:19:02 +0800

    Changes

--- Study/day_04_3.java
+++ Study/day_04_3.java
@@ -6,35 +6,36 @@ import java.util.Scanner;
 public class day_04_3 {
     public static void main(String[] args) {
         while (true){
-            System.out.println("输入你要查找的数:"+" ");
+            int[] arr=createArr(20,20);
+            System.out.print("输入你要查找的数:"+" ");
             Scanner s=new Scanner(System.in);
             int n=s.nextInt();
-            exist(createArr(20,20),n);
-            iterate(createArr(20,20));
+            exist(arr,n);
+            boolean flag=exist(arr,20);
+            System.out.println(flag);
+            iterate(arr);
+
         }
 
 
     }
     //判断数组中某一个数是否存，将结果返回给调用出
-    public static int exist(int[] a,int b){
-        int e=0;
+    public static boolean exist(int[] a,int b){
+
         for (int i=0; i < a.length; i++) {
             if (a[i]==b){
-                e=a[i];
-                System.out.print("存在"+" "+e);
+                int e=a[i];
+                return true;
 
             }
         }
-        if (e!=b){
-            System.out.print("不存在");
-        }
-        return e;
+        return false;
     }
     public static int[] createArr(int a,int b) {
         Random r = new Random();
         int[] arr = new int[b];
-        int m=1;
-        for (int i = 1; i < arr.length; i++) {
+        int m;
+        for (int i = 0; i < arr.length; i++) {
                 m = r.nextInt(a) + 1;
                 arr[i] = m;
         }
