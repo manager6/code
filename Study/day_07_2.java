e36e0450985226db1e7850da747e1146a6ec30f8
Tomato <84717386+manager6@users.noreply.github.com>
2022-11-09 22:33:24 +0800

    Changes

--- Study/day_07_2.java
+++ Study/day_07_2.java
@@ -2,6 +2,12 @@ package Study;
 //复制数组
 public class day_07_2 {
     public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int[] newarr=new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i]=arr[i];
+            System.out.print(newarr[i]+" ");
+        }
 
     }
 }
